f7ec5371ab5a2f98ddbf5e876ed8d51b556f5927
Irvin Gonzalez <73666766+Irvin-g@users.noreply.github.com>
2024-05-25 23:25:30 -0600

    Add files via upload

---
+++ Guia3/Guia3/Guia3/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace Guia3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelDibujo = new System.Windows.Forms.Panel();
+            this.btnAgregarNodo = new System.Windows.Forms.Button();
+            this.btnAgregarArco = new System.Windows.Forms.Button();
+            this.btnEjecutarDijkstra = new System.Windows.Forms.Button();
+            this.txtNodoInicial = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstResultados = new System.Windows.Forms.ListBox();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.lblRutaCorta = new System.Windows.Forms.Label();
+            this.btnCalcularRuta = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panelDibujo
+            // 
+            this.panelDibujo.Location = new System.Drawing.Point(366, 22);
+            this.panelDibujo.Name = "panelDibujo";
+            this.panelDibujo.Size = new System.Drawing.Size(362, 383);
+            this.panelDibujo.TabIndex = 0;
+            // 
+            // btnAgregarNodo
+            // 
+            this.btnAgregarNodo.Location = new System.Drawing.Point(16, 69);
+            this.btnAgregarNodo.Name = "btnAgregarNodo";
+            this.btnAgregarNodo.Size = new System.Drawing.Size(97, 31);
+            this.btnAgregarNodo.TabIndex = 1;
+            this.btnAgregarNodo.Text = "Agregar nodos";
+            this.btnAgregarNodo.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregarArco
+            // 
+            this.btnAgregarArco.Location = new System.Drawing.Point(134, 69);
+            this.btnAgregarArco.Name = "btnAgregarArco";
+            this.btnAgregarArco.Size = new System.Drawing.Size(100, 31);
+            this.btnAgregarArco.TabIndex = 2;
+            this.btnAgregarArco.Text = "Agregar arcos";
+            this.btnAgregarArco.UseVisualStyleBackColor = true;
+            // 
+            // btnEjecutarDijkstra
+            // 
+            this.btnEjecutarDijkstra.Location = new System.Drawing.Point(240, 150);
+            this.btnEjecutarDijkstra.Name = "btnEjecutarDijkstra";
+            this.btnEjecutarDijkstra.Size = new System.Drawing.Size(99, 25);
+            this.btnEjecutarDijkstra.TabIndex = 3;
+            this.btnEjecutarDijkstra.Text = "Ejecutar";
+            this.btnEjecutarDijkstra.UseVisualStyleBackColor = true;
+            // 
+            // txtNodoInicial
+            // 
+            this.txtNodoInicial.Location = new System.Drawing.Point(134, 155);
+            this.txtNodoInicial.Name = "txtNodoInicial";
+            this.txtNodoInicial.Size = new System.Drawing.Size(100, 20);
+            this.txtNodoInicial.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ingresar el nodo inicial:";
+            // 
+            // lstResultados
+            // 
+            this.lstResultados.FormattingEnabled = true;
+            this.lstResultados.Location = new System.Drawing.Point(16, 184);
+            this.lstResultados.Name = "lstResultados";
+            this.lstResultados.Size = new System.Drawing.Size(218, 134);
+            this.lstResultados.TabIndex = 6;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(240, 295);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(99, 23);
+            this.btnLimpiar.TabIndex = 7;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // lblRutaCorta
+            // 
+            this.lblRutaCorta.AutoSize = true;
+            this.lblRutaCorta.Location = new System.Drawing.Point(16, 348);
+            this.lblRutaCorta.Name = "lblRutaCorta";
+            this.lblRutaCorta.Size = new System.Drawing.Size(35, 13);
+            this.lblRutaCorta.TabIndex = 8;
+            this.lblRutaCorta.Text = "label2";
+            // 
+            // btnCalcularRuta
+            // 
+            this.btnCalcularRuta.Location = new System.Drawing.Point(240, 215);
+            this.btnCalcularRuta.Name = "btnCalcularRuta";
+            this.btnCalcularRuta.Size = new System.Drawing.Size(99, 23);
+            this.btnCalcularRuta.TabIndex = 9;
+            this.btnCalcularRuta.Text = "Calcular Ruta";
+            this.btnCalcularRuta.UseVisualStyleBackColor = true;
+            this.btnCalcularRuta.Click += new System.EventHandler(this.btnCalcularRuta_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(740, 417);
+            this.Controls.Add(this.btnCalcularRuta);
+            this.Controls.Add(this.lblRutaCorta);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.lstResultados);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNodoInicial);
+            this.Controls.Add(this.btnEjecutarDijkstra);
+            this.Controls.Add(this.btnAgregarArco);
+            this.Controls.Add(this.btnAgregarNodo);
+            this.Controls.Add(this.panelDibujo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelDibujo;
+        private System.Windows.Forms.Button btnAgregarNodo;
+        private System.Windows.Forms.Button btnAgregarArco;
+        private System.Windows.Forms.Button btnEjecutarDijkstra;
+        private System.Windows.Forms.TextBox txtNodoInicial;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lstResultados;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label lblRutaCorta;
+        private System.Windows.Forms.Button btnCalcularRuta;
+    }
+}
+
---
+++ Guia3/Guia3/Guia3/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Guia3
+{
+    public partial class Form1 : Form
+    {
+        private List<Nodo> nodos = new List<Nodo>();
+        private List<Arco> arcos = new List<Arco>();
+        private int nodoId = 0;
+        private Nodo nodoSeleccionado = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Conectar eventos manualmente
+            this.btnAgregarNodo.Click += new EventHandler(this.btnAgregarNodo_Click);
+            this.btnAgregarArco.Click += new EventHandler(this.btnAgregarArco_Click);
+            this.btnEjecutarDijkstra.Click += new EventHandler(this.btnEjecutarDijkstra_Click);
+            this.btnLimpiar.Click += new EventHandler(this.btnLimpiar_Click);
+            this.panelDibujo.Paint += new PaintEventHandler(this.panelDibujo_Paint);
+            this.btnCalcularRuta.Click += new EventHandler(this.btnCalcularRuta_Click); // Conectar el evento del botón de calcular ruta
+        }
+
+        private void btnAgregarNodo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Haz clic en el panel para agregar un nodo");
+            panelDibujo.MouseClick += PanelDibujo_MouseClick;
+        }
+
+        private void PanelDibujo_MouseClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show($"Se agregó un nodo en ({e.Location.X}, {e.Location.Y})");
+            nodos.Add(new Nodo(nodoId++, e.Location));
+            panelDibujo.Invalidate();
+            panelDibujo.MouseClick -= PanelDibujo_MouseClick;
+        }
+
+        private void btnAgregarArco_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Selecciona dos nodos para agregar un arco");
+            panelDibujo.MouseClick += SeleccionarNodoParaArco;
+        }
+
+        private void SeleccionarNodoParaArco(object sender, MouseEventArgs e)
+        {
+            var nodo = nodos.FirstOrDefault(n => EsNodoSeleccionado(n, e.Location));
+            if (nodo != null)
+            {
+                if (nodoSeleccionado == null)
+                {
+                    nodoSeleccionado = nodo;
+                    MessageBox.Show($"Nodo {nodo.Id} seleccionado. Ahora selecciona el nodo de destino.");
+                }
+                else
+                {
+                    int peso = PromptForPeso();
+                    if (peso > 0)
+                    {
+                        arcos.Add(new Arco(nodoSeleccionado, nodo, peso));
+                        MessageBox.Show($"Arco agregado desde {nodoSeleccionado.Id} hasta {nodo.Id} con peso {peso}");
+                        nodoSeleccionado = null;
+                        panelDibujo.Invalidate();
+                        panelDibujo.MouseClick -= SeleccionarNodoParaArco;
+                    }
+                }
+            }
+        }
+
+        private bool EsNodoSeleccionado(Nodo nodo, Point punto)
+        {
+            int radio = 10;
+            return Math.Sqrt(Math.Pow(nodo.Posicion.X - punto.X, 2) + Math.Pow(nodo.Posicion.Y - punto.Y, 2)) <= radio;
+        }
+
+        private void btnEjecutarDijkstra_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtNodoInicial.Text, out int nodoInicial) && nodoInicial >= 0 && nodoInicial < nodos.Count)
+            {
+                MessageBox.Show($"Ejecutando Dijkstra desde el nodo {nodoInicial}");
+                var algoritmo = new AlgoritmoDijkstra(nodos, arcos);
+                algoritmo.Ejecutar(nodoInicial);
+
+                var distancias = algoritmo.ObtenerDistancias();
+                MostrarDistancias(distancias);
+            }
+            else
+            {
+                MessageBox.Show("Por favor, ingresa un nodo inicial válido.");
+            }
+        }
+
+        private void MostrarDistancias(int[] distancias)
+        {
+            lstResultados.Items.Clear();
+            for (int i = 0; i < distancias.Length; i++)
+            {
+                lstResultados.Items.Add($"Distancia al nodo {i}: {distancias[i]}");
+            }
+            MessageBox.Show("Distancias calculadas y mostradas.");
+        }
+
+        private void panelDibujo_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            foreach (var nodo in nodos)
+            {
+                g.FillEllipse(Brushes.Blue, nodo.Posicion.X - 5, nodo.Posicion.Y - 5, 10, 10);
+                g.DrawString(nodo.Id.ToString(), DefaultFont, Brushes.White, nodo.Posicion);
+            }
+
+            foreach (var arco in arcos)
+            {
+                g.DrawLine(Pens.Black, arco.Origen.Posicion, arco.Destino.Posicion);
+                var puntoMedio = new Point((arco.Origen.Posicion.X + arco.Destino.Posicion.X) / 2, (arco.Origen.Posicion.Y + arco.Destino.Posicion.Y) / 2);
+                g.DrawString(arco.Peso.ToString(), DefaultFont, Brushes.Red, puntoMedio);
+            }
+        }
+
+        private int PromptForPeso()
+        {
+            using (var form = new Form())
+            {
+                var lbl = new Label() { Left = 50, Top = 20, Text = "Peso del arco:" };
+                var txt = new TextBox() { Left = 50, Top = 50, Width = 200 };
+                var btnOk = new Button() { Text = "OK", Left = 150, Width = 100, Top = 80, DialogResult = DialogResult.OK };
+
+                form.Controls.Add(lbl);
+                form.Controls.Add(txt);
+                form.Controls.Add(btnOk);
+                form.AcceptButton = btnOk;
+
+                if (form.ShowDialog() == DialogResult.OK && int.TryParse(txt.Text, out int peso))
+                {
+                    return peso;
+                }
+                else
+                {
+                    MessageBox.Show("Por favor, ingresa un peso válido.");
+                    return 0;
+                }
+            }
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            // Limpiar nodos y arcos
+            nodos.Clear();
+            arcos.Clear();
+            nodoId = 0;
+            nodoSeleccionado = null;
+
+            // Limpiar cuadro de texto
+            txtNodoInicial.Text = string.Empty;
+
+            // Limpiar ListBox
+            lstResultados.Items.Clear();
+
+            // Limpiar el panel de dibujo
+            panelDibujo.Invalidate();
+        }
+
+        private void btnCalcularRuta_Click(object sender, EventArgs e)
+        {
+            if (lstResultados.Items.Count == 0)
+            {
+                MessageBox.Show("No hay distancias calculadas.");
+                return;
+            }
+
+            int menorDistancia = int.MaxValue;
+
+            foreach (var item in lstResultados.Items)
+            {
+                string[] partes = item.ToString().Split(':');
+                if (partes.Length == 2 && int.TryParse(partes[1].Trim(), out int distancia) && distancia > 0)
+                {
+                    if (distancia < menorDistancia)
+                    {
+                        menorDistancia = distancia;
+                    }
+                }
+            }
+
+            if (menorDistancia == int.MaxValue)
+            {
+                MessageBox.Show("No se encontraron distancias ");
+            }
+            else
+            {
+                MessageBox.Show($"La menor distancia mas corta es: {menorDistancia}");
+            }
+        }
+    }
+
+    public class Nodo
+    {
+        public int Id { get; set; }
+        public Point Posicion { get; set; }
+
+        public Nodo(int id, Point posicion)
+        {
+            Id = id;
+            Posicion = posicion;
+        }
+    }
+
+    public class Arco
+    {
+        public Nodo Origen { get; set; }
+        public Nodo Destino { get; set; }
+        public int Peso { get; set; }
+
+        public Arco(Nodo origen, Nodo destino, int peso)
+        {
+            Origen = origen;
+            Destino = destino;
+            Peso = peso;
+        }
+    }
+
+    public class AlgoritmoDijkstra
+    {
+        private List<Nodo> nodos;
+        private List<Arco> arcos;
+        private int[] distancias;
+        private int[] predecesores;
+
+        public AlgoritmoDijkstra(List<Nodo> nodos, List<Arco> arcos)
+        {
+            this.nodos = nodos;
+            this.arcos = arcos;
+            distancias = new int[nodos.Count];
+            predecesores = new int[nodos.Count];
+        }
+
+        public void Ejecutar(int nodoInicial)
+        {
+            for (int i = 0; i < nodos.Count; i++)
+            {
+                distancias[i] = int.MaxValue;
+                predecesores[i] = -1;
+            }
+            distancias[nodoInicial] = 0;
+
+            var colaPrioridad = new SortedSet<Tuple<int, int>>(Comparer<Tuple<int, int>>.Create((x, y) => x.Item2 == y.Item2 ? x.Item1.CompareTo(y.Item1) : x.Item2.CompareTo(y.Item2)));
+            colaPrioridad.Add(Tuple.Create(nodoInicial, 0));
+
+            while (colaPrioridad.Any())
+            {
+                var (nodoActual, distanciaActual) = colaPrioridad.Min;
+                colaPrioridad.Remove(colaPrioridad.Min);
+
+                foreach (var arco in arcos.Where(a => a.Origen.Id == nodoActual))
+                {
+                    int nuevoCosto = distanciaActual + arco.Peso;
+                    if (nuevoCosto < distancias[arco.Destino.Id])
+                    {
+                        colaPrioridad.Remove(Tuple.Create(arco.Destino.Id, distancias[arco.Destino.Id]));
+                        distancias[arco.Destino.Id] = nuevoCosto;
+                        predecesores[arco.Destino.Id] = nodoActual;
+                        colaPrioridad.Add(Tuple.Create(arco.Destino.Id, nuevoCosto));
+                    }
+                }
+            }
+        }
+
+        public int[] ObtenerDistancias() => distancias;
+    }
+}
